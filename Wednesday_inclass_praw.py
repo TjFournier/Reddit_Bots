eb4c47b55c35ecd526ecd0cb89f5780e2a6a4665
TjFournier <tjfournier19@gmail.com>
2021-11-10 16:54:01 -0800

    Update Wednesday_inclass_praw.py

--- Wednesday_inclass_praw.py
+++ Wednesday_inclass_praw.py
@@ -4,3 +4,6 @@ reddit = praw.reddit
 
 url = "https://www.reddit.com/r/funny/comments/3g1jfi/buttons/"
 submission = reddit.submission(url=url)
+
+for top_level_comment in submission.comments:
+    print(top_level_comment.body)
