3193adff675f98ac79391c9dbb3065b753336754
TjFournier <tjfournier19@gmail.com>
2021-11-15 17:09:35 -0800

    u

--- Wednesday_inclass_praw.py
+++ Wednesday_inclass_praw.py
@@ -1,10 +1,10 @@
 import praw
-
-reddit = praw.reddit("bot0")
+import time
+reddit = praw.reddit('bot0',user_agent='cs_40')
 
 url = "https://www.reddit.com/r/funny/comments/3g1jfi/buttons/"
 submission = reddit.submission(url=url)
-'''
+
+
 for top_level_comment in submission.comments:
     print(top_level_comment.body)
-'''
--- bot_stats.py
+++ bot_stats.py
@@ -8,7 +8,7 @@ import pprint
 # WARNING:
 # If you include any credential information in your final submission,
 # you will receive NEGATIVE POINTS on your lab!!!
-reddit = praw.Reddit()
+reddit = praw.Reddit('bot0',user_agent='cs_40')
 
 # connect to the "Main Discussion Thread" reddit submission
 submission = reddit.submission(url='https://old.reddit.com/r/BotTown/comments/qqmr8l/main_discussion_thread/')
@@ -30,14 +30,66 @@ submission.comments.replace_more(limit=None)
 # 2. The total number of deleted top level comments,
 # 3. The total number of comments sent by each user.
 #    You should use a dictionary where the keys are the username and the values are the total number of comments.
+
+redditors = {}
+authors = []
+nondelcom = 0
+delcom = 0
+numauthor = 0
+
+toplvlcom = list(submission.comments)
+for i in range(len(toplvlcom)):
+    numauthor = 0
+    author = submission.comments[i].author
+    if author:
+        nondelcom += 1
+        authors.append(author)
+        for a in authors:
+            if author == a:
+                numauthor += 1
+    else:
+        delcom += 1
+    if author:
+        redditors[author] = numauthor
+
 print('='*40)
 print('top level comments')
 print('='*40)
+print('nondeleted _comments=',nondelcom)
+print('deleted_comments=',delcom)
+pprint.pprint(redditors)
+
 
 # FIXME:
 # Repeat the calculations above,
 # but do it for ALL comments,
 # not just top level comments.
+
+redditors = {}
+authors = []
+nondelcom = 0
+delcom = 0
+numauthor = 0
+
+allcom = submission.comments.list()
+for comment in submission.comments.list():
+    author = comment.author
+    numauthor = 0
+    if author:
+        nondelcom += 1
+        authors.append(author)
+        for a in authors:
+            if author == a:
+                numauthor += 1
+    else:
+        delcom += 1
+    if author:
+        redditors[author] = numauthor
+
 print('='*40)
-print('all comments')
+print('top level comments')
 print('='*40)
+print('nondeleted _comments=',nondelcom)
+print('deleted_comments=',delcom)
+pprint.pprint(redditors)
+
